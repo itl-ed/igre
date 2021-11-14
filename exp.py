c60f784e9c623e5bcf67b7ac5f9860e4bdd5fd8c
rimvydasrub <rimvydasrub@gmail.com>
2021-11-14 22:27:48 +0000

    exp

--- exp.py
+++ exp.py
@@ -3,17 +3,22 @@ from argparse import ArgumentParser
 import random
 from datetime import datetime
 from collections import defaultdict
+from itertools import product
 from igre.logic.model import DomainModel, Denotation, Entities
 
 import torch
 from torch import  Tensor
 
 import wandb
-from igre.logic.syntax import RefExp
+from igre.logic.syntax import AtomicSentence, NegatedSentence, OrSentence, RefExp
 
 from igre.utils import ShapeWorldDataset
 from igre.grounder import Grounder
 from igre.reasoner import Reasoner
+from igre.logic import LogicRefExpParser
+from igre.lang import RefExpParser
+
+
 
 
 
@@ -45,10 +50,12 @@ def prediction(entities: Entities,
 
 def metrics(model_true: DomainModel,
             model_pred: DomainModel,
-            refexps: List[RefExp],
-            flag: str="") -> Dict[str, float]:
+            refexps: List[RefExp]) -> Dict[str, float]:
     """evaluation metrics for 
     precision, recall, f1 for model and reference prediction"""
+    C1 = ['red', 'blue', 'magenta', 'yellow', 'green', 'cyan']
+    S1 = ['square', 'circle', 'triangle', 'pentagon', 'cross', 'ellipse', 'semicircle']
+    R2 = ['left', 'right', 'above', 'below']
 
     def prf(pred: set, real: set) -> Tuple[float, float, float]:
 
@@ -63,21 +70,31 @@ def metrics(model_true: DomainModel,
 
         return sum(elems)/len(elems) if len(elems) > 0 else 0
 
-    model_ps, model_rs, model_fs = [],[],[]
-    for symbol in model_pred.extension.keys():
+    def eval_fpr(model_pred, model_true, symbols: List[str]):
 
-        if symbol == 'object':
-            continue
+        model_ps, model_rs, model_fs = [],[],[]
+        for symbol in model_pred.extension.keys():
 
-        predicted = model_pred.extension[symbol]
-        ground = model_true.extension[symbol]
+            if symbol not in symbols:
+                continue
 
-        p,r, f = prf(predicted, ground)
-        model_ps.append(p)
-        model_rs.append(r)
-        model_fs.append(f)
+            predicted = model_pred.extension[symbol]
+            ground = model_true.extension[symbol]
+
+            p,r, f = prf(predicted, ground)
+            model_ps.append(p)
+            model_rs.append(r)
+            model_fs.append(f)
+
+        return model_ps, model_rs, model_fs
+
+    full_ps, full_rs, full_fs = eval_fpr(model_pred, model_true, C1+S1+R2)
+    c1_ps, c1_rs, c1_fs = eval_fpr(model_pred, model_true, C1)
+    s1_ps, s1_rs, s1_fs = eval_fpr(model_pred, model_true, S1)
+    r2_ps, r2_rs, r2_fs = eval_fpr(model_pred, model_true, R2)
 
     ref_ps, ref_rs, ref_fs = [],[],[]
+
     for refexp in refexps:
 
         predicted = model_true.denotations(refexp.snt, refexp.var)
@@ -87,19 +104,21 @@ def metrics(model_true: DomainModel,
         ref_rs.append(r)
         ref_fs.append(f)
 
-    print(f"model ps: {model_ps}")
-    print(f"model rs: {model_rs}")
-    print(f"model fs: {model_fs}")
-    print(f"ref ps: {ref_ps}")
-    print(f"ref rs: {ref_rs}")
-    print(f"ref fs: {ref_fs}")
-
-    return {f"{flag}model_p": save_avg(model_ps), 
-            f"{flag}model_r": save_avg(model_rs),
-            f"{flag}model_f": save_avg(model_fs),
-            f"{flag}reference_p": save_avg(ref_ps), 
-            f"{flag}reference_r": save_avg(ref_rs),
-            f"{flag}reference_f": save_avg(ref_fs),
+    return {f"ful_p": save_avg(full_ps), 
+            f"ful_r": save_avg(full_rs),
+            f"ful_f": save_avg(full_fs),
+            f"c1_p": save_avg(c1_ps), 
+            f"c1_r": save_avg(c1_rs),
+            f"c1_f": save_avg(c1_fs),
+            f"s1_p": save_avg(s1_ps), 
+            f"s1_r": save_avg(s1_rs),
+            f"s1_f": save_avg(s1_fs),
+            f"r2_p": save_avg(r2_ps), 
+            f"r2_r": save_avg(r2_rs),
+            f"r2_f": save_avg(r2_fs),
+            f"ref_p": save_avg(ref_ps), 
+            f"ref_r": save_avg(ref_rs),
+            f"ref_f": save_avg(ref_fs),
             }
 
 
@@ -108,12 +127,21 @@ def evaluation(config: wandb.Config,
             prop_grounder: Grounder,
             rels_grounder: Grounder,) -> None:
 
-    results = {"test_model_p": 0, 
-            "test_model_r": 0,
-            "test_model_f": 0,
-            "test_reference_p": 0, 
-            "test_reference_r": 0,
-            "test_reference_f": 0,
+    results = {f"ful_p": 0, 
+            f"ful_r": 0,
+            f"ful_f": 0,
+            f"c1_p": 0, 
+            f"c1_r": 0,
+            f"c1_f": 0,
+            f"s1_p": 0, 
+            f"s1_r": 0,
+            f"s1_f": 0,
+            f"r2_p": 0, 
+            f"r2_r": 0,
+            f"r2_f": 0,
+            f"ref_p": 0, 
+            f"ref_r": 0,
+            f"ref_f": 0,
             }
 
     for prop_features,rels_features,refexps, model_true in dset:
@@ -126,7 +154,7 @@ def evaluation(config: wandb.Config,
                             rels_grounder,
                             config.rels_threshold)
 
-        model_results = metrics(model_true, model_pred, refexps, flag="test_")
+        model_results = metrics(model_true, model_pred, refexps)
 
         for result in results.keys():
             results[result] += model_results[result]
@@ -140,6 +168,12 @@ def main(config):
 
     random.seed(config.seed)
     torch.manual_seed(config.seed)
+
+    lang = RefExpParser(grm_path=config.grm_path,
+                        ace_path=config.ace_path,
+                        utool_path=config.utool_path)
+    logic = LogicRefExpParser()
+    parser = lambda x: logic(lang(x))
 
     train = ShapeWorldDataset(data_path = config.train_path,
                             feature_extractor = config.feature_extractor,
@@ -175,7 +209,9 @@ def main(config):
     
     train_iter, total_obs = 0, 0
 
-    for  prop_features, rels_features, refexps, model_true in train:
+    for idx, (prop_features, rels_features, refexps, model_true) in enumerate(train):
+
+        print(f"domain model #{idx}")
 
         # re-index entitities in the domain model
         total_obs, index = model_true.reindex(total_obs)
@@ -205,20 +241,23 @@ def main(config):
 
             prop_grounder.update_values(prop_values)
             rels_grounder.update_values(rels_values)
-
-            model_pred = prediction(model_true.entities,
-                                prop_features,
-                                prop_grounder,
-                                config.prop_threshold,
-                                rels_features,
-                                rels_grounder,
-                                config.rels_threshold)
             
-            result = metrics(model_true, model_pred, [refexp], flag="train_")
-            wandb.log(result)
-
             evaluation(config, test, prop_grounder, rels_grounder)
-            reasoner.add_exist(refexp, model_true)
+            reasoner.add_full(refexp, model_true)
+            ## add axioms for relations
+            for rel in rels_grounder.symbols:
+                for term in product(model_true.entities, repeat=1):
+                    terms = [term[0], term[0]]
+                    irr = NegatedSentence(AtomicSentence(rel, terms))
+                    reasoner.add_sentence(irr)
+
+                for terms in product(model_true.entities, repeat=2):
+                    pos_terms = list(terms)
+                    neg_terms = list(reversed(terms))
+                    antisym = OrSentence(left=NegatedSentence(AtomicSentence(rel, pos_terms)),
+                    right=NegatedSentence(AtomicSentence(rel, neg_terms))) 
+
+                    reasoner.add_sentence(antisym)
 
             # batch update
             if train_iter != 1 and train_iter % config.batch_freq == 0:
@@ -279,7 +318,12 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     ## initialize experiments
-    wandb.init(project='igre', entity='rimvydasrub', name='exitsts'+str(datetime.now()))
+    wandb.login(key="960dbf7b3effe15f5578aa067e9c88d77f076f51")
+    wandb.init(project='igre', 
+            entity='rimvydasrub',
+            group="exp_final",
+            dir = "./wandb_tmp/",
+            name='full-axioms_'+str(datetime.now()))
     config = wandb.config
     config.update(args)
 
